e4a6e03052c55820998fbe59ffc8f5f85bae180b
kyyneogs <oks990304@gmail.com>
2024-05-08 20:11:59 +0900

    solved : 2839 설탕 배달

---
+++ cpp/class-2/2839 설탕 배달.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  for (int i = N / 5; i >= 0; i--) {
+    for (int j = (N - 5 * i) / 3; j >= 0; j--) {
+      if (5 * i + 3 * j == N) {
+        cout << i + j;
+        return 0;
+      }
+    }
+  }
+  cout << -1;
+}
