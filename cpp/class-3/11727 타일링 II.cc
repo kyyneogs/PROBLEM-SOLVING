80b59a8ca0c756445165b71b6cdfbb3ccd70be7e
kyyneogs <oks990304@gmail.com>
2024-05-27 22:03:25 +0900

    solved : 11727 타일링 II

---
+++ cpp/class-3/11727 타일링 II.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int *arr = new int[n + 1];
+
+  arr[1] = 1;
+  if (n > 1) arr[2] = 3;
+
+  for (int i = 3; i <= n; ++i) {
+    arr[i] = (arr[i - 2] * 2 + arr[i - 1]) % 10007;
+  }
+  cout << arr[n];
+  delete[] arr;
+}
